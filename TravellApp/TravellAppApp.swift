3f490d6ee27bffac0d428526d71eef55898feb1e
Vladimir Iusupov <vn.yusupov@gmail.com>
2025-08-27 11:44:43 +0300

    Initial Commit

---
+++ TravellApp/TravellAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravellAppApp.swift
+//  TravellApp
+//
+//  Created by Vladimir Iusupov on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravellAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
